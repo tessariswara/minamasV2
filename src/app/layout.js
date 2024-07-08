1b1ed014ba26ca0628b2a1e01b9de02c66f708e3
tessafauzi <tessa.projectmast@outlook.com>
2024-07-08 09:20:32 +0700

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import './globals.css';
+
+// PROJECT IMPORTS
+import ProviderWrapper from 'store/ProviderWrapper';
+
+export const metadata = {
+  title: 'Fuel Management System',
+  description: 'This is Dashboard Management.'
+};
+
+// ==============================|| ROOT LAYOUT ||============================== //
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ProviderWrapper>{children}</ProviderWrapper>
+      </body>
+    </html>
+  );
+}
+
+RootLayout.propTypes = {
+  children: PropTypes.node
+};
---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+// // project import
+// import MinimalLayout from 'layout/MinimalLayout';
+// import Landing from 'views/landing';
+
+// // ==============================|| HOME PAGE ||============================== //
+
+// export default function HomePage() {
+//   return (
+//     <MinimalLayout>
+//       <Landing />
+//     </MinimalLayout>
+//   );
+// }
+
+import MinimalLayout from 'layout/MinimalLayout';
+import Login from 'views/authentication/auth3/login';
+
+// ==============================|| PAGE ||============================== //
+
+export default function HomePage() {
+  return <Login />;
+}
